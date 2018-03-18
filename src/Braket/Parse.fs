e8227b6765517cabd421130025939e7c8dcb65c3
charlie <the_charlie_daly@hotmail.co.uk>
2018-03-18 10:37:11 +0800

    Problems

--- src/Braket/Parse.fs
+++ src/Braket/Parse.fs
@@ -1,5 +1,7 @@
 module Parse
 
+open FParsec
+
 type Statement = 
     | Nil
     | Bra of string
@@ -7,3 +9,17 @@ type Statement =
     | Scalar of string
     | Add of Statement * Statement
     | Product of Statement * Statement
+
+let mapSuccess: Parser<'a, 'u> -> ('a -> 'b) -> Parser<'b, 'u> = fun parser mapper -> parse {
+    let! value = parser
+    return value |> mapper }
+
+// let (</>) = mapSuccess
+
+let pbra = pstring "<" >>. asciiLetter .>> pstring "|" 
+
+let pket = pstring "|" >>. asciiLetter .>> pstring ">" </> (fun c -> c.ToString() |> Ket)
+
+let pproductKet = asciiLetter .>> pstring ">" </> (fun c -> c.ToString() |> Ket)
+
+// let pbraketProduct = pipe2 pbra pproductKet (fun bra ket -> Product(bra, ket))
--- tests/Braket.Tests/FParsecTests.fs
+++ tests/Braket.Tests/FParsecTests.fs
@@ -15,27 +15,13 @@ let ``Parse simple float`` () =
 
 open Parse
 
-let mapSuccess: Parser<'a, 'u> -> ('a -> 'b) -> Parser<'b, 'u> = fun parser mapper -> parse {
-    let! value = parser
-    return value |> mapper }
-
-let (</>) = mapSuccess
-
-let pbra = pstring "<" >>. asciiLetter .>> pstring "|" </> (fun c -> c.ToString() |> Bra)
-
 [<Fact>]
 let ``Parse simple bra`` () = 
     test pbra "<A|" |> should equal <| Bra "A"
 
-let pket = pstring "|" >>. asciiLetter .>> pstring ">" </> (fun c -> c.ToString() |> Ket)
-
 [<Fact>]
 let ``Parse simple ket`` () = 
     test pket "|A>" |> should equal <| Ket "A"
-
-let pproductKet = asciiLetter .>> pstring ">" </> (fun c -> c.ToString() |> Ket)
-
-let pbraketProduct = pipe2 pbra pproductKet (fun bra ket -> Product(bra, ket))
 
 [<Fact>]
 let ``Parse braket product`` () = 
